67eb7f2eb4346efc04d1ba241c3acbfeef0a6136
natane010 <mimikaumi@gmail.com>
2024-05-13 00:03:24 +0900

    物理パッケージ追加

---
+++ Assets/TKPackages/TKPhysics/Editor/ADBRunrimePointEditor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace TKPackages.TKPhysics.Editor
+{
+    using Runtime;
+    [CustomEditor(typeof(ADBRuntimePoint))]
+    public class ADBRunrimePointEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI() { } //Hide
+    }
+
+}
